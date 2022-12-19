e07d3f6026ac290e74312edfa9940014e3639977
AKANDE Ayobami <83408171+adeyemimichael@users.noreply.github.com>
2022-12-19 13:48:25 +0100

    Add files via upload
    I added a new style sheet to add some styling to the button

---
+++ script.js
@@ -0,0 +1,16 @@
+const QuoteText = document.querySelector("#quote" );
+quoteBtn = document.querySelector("button");
+authorName = document.querySelector(".author-div #author")
+soundBtn = document.querySelector("#sound-track")
+function RandomQuote (){  
+fetch("http://api.quotable.io/random")   .then(res => res.json())
+.then(result => {
+  QuoteText.innerText=result.content;
+  authorName.innerText=result.author;
+});
+} 
+soundBtn.addEventListener("click",() => { 
+  let Utterance = new SpeechSynthesisUtterance(`${QuoteText.innerText }  by ${authorName.innerText}`);
+  speechSynthesis.speak(Utterance);
+})
+quoteBtn.addEventListener("click", RandomQuote)
