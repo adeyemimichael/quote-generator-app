c93ab531cd00bfe536fac88fdc437ec77475a622
AKANDE Ayobami <83408171+adeyemimichael@users.noreply.github.com>
2022-12-19 14:10:31 +0100

    Update script.js
    checked the api

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ quoteBtn = document.querySelector("button");
 authorName = document.querySelector(".author-div #author")
 soundBtn = document.querySelector("#sound-track")
 function RandomQuote (){  
-fetch("http://api.quotable.io/random")   .then(res => res.json())
+fetch("https://api.quotable.io/random")   .then(res => res.json())
 .then(result => {
   QuoteText.innerText=result.content;
   authorName.innerText=result.author;
